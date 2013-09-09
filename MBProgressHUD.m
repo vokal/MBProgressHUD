347d804b908a37e297ec2b6079e65136bf9e01db
teejay <tfallon@mail.depaul.edu>
2013-09-09 16:27:14 -0500

    Updating for iOS7

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -514,7 +514,7 @@ static const CGFloat kDetailsLabelFontSize = 12.f;
 	totalSize.width = MAX(totalSize.width, indicatorF.size.width);
 	totalSize.height += indicatorF.size.height;
 	
-	CGSize labelSize = [label.text sizeWithFont:label.font];
+	CGSize labelSize = [label.text sizeWithAttributes:@{NSFontAttributeName : label.font}];
 	labelSize.width = MIN(labelSize.width, maxWidth);
 	totalSize.width = MAX(totalSize.width, labelSize.width);
 	totalSize.height += labelSize.height;
@@ -524,8 +524,12 @@ static const CGFloat kDetailsLabelFontSize = 12.f;
 
 	CGFloat remainingHeight = bounds.size.height - totalSize.height - kPadding - 4 * margin; 
 	CGSize maxSize = CGSizeMake(maxWidth, remainingHeight);
-	CGSize detailsLabelSize = [detailsLabel.text sizeWithFont:detailsLabel.font 
-								constrainedToSize:maxSize lineBreakMode:detailsLabel.lineBreakMode];
+	CGSize detailsLabelSize = [detailsLabel.text sizeWithAttributes:@{NSFontAttributeName : detailsLabel.font,
+                                                                      }];
+    if (detailsLabelSize.width > maxSize.width) {
+        detailsLabelSize.width = maxSize.width;
+    }
+    
 	totalSize.width = MAX(totalSize.width, detailsLabelSize.width);
 	totalSize.height += detailsLabelSize.height;
 	if (detailsLabelSize.height > 0.f && (indicatorF.size.height > 0.f || labelSize.height > 0.f)) {
